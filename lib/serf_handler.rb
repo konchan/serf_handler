99ae1671d860a2a8d20a0a2814c0fb1658557932
konchan <konno.katsuyuki@nifty.com>
2014-08-02 16:57:08 +0900

    fixed indentation of source code

--- lib/serf_handler.rb
+++ lib/serf_handler.rb
@@ -4,54 +4,54 @@
 require "logger"
 
 class SerfHandler
-	attr :name, :role, :event
+  attr :name, :role, :event
 
-	def initialize(logger=nil)
-		if logger
-			@logger = logger
-		else
-			@logger = Logger.new(STDOUT)
-			@logger.level = Logger::INFO
-		end
-		@name = ENV['SERF_SELF_NAME']
-		@role = ENV['SERF_TAG_ROLE'] || ENV['SERF_SELF_ROLE']
-		@event = ENV['SERF_EVENT'] == 'user'? ENV['SERF_USER_EVENT'] : ENV['SERF_EVENT'].gsub(/-/, '_')
-	end
+  def initialize(logger=nil)
+    if logger
+      @logger = logger
+    else
+      @logger = Logger.new(STDOUT)
+      @logger.level = Logger::INFO
+    end
+    @name = ENV['SERF_SELF_NAME']
+    @role = ENV['SERF_TAG_ROLE'] || ENV['SERF_SELF_ROLE']
+    @event = ENV['SERF_EVENT'] == 'user'? ENV['SERF_USER_EVENT'] : ENV['SERF_EVENT'].gsub(/-/, '_')
+  end
 
-	def log(msg)
-		@logger.info(msg)
-	end
+  def log(msg)
+    @logger.info(msg)
+  end
 end
 
 class SerfHandlerProxy < SerfHandler
-	def initialize(logger=nil)
-		super(logger)
-		@handlers = {}
-	end
+  def initialize(logger=nil)
+    super(logger)
+    @handlers = {}
+  end
 
-	def register(role, handler)
-		@handlers[role] = handler
-	end
+  def register(role, handler)
+    @handlers[role] = handler
+  end
 
-	def get_klass()
-		klass = false
-		if @handlers.include?(@role)
-			klass = @handlers[@role]
-		elsif @handlers.include?('default')
-			klass = @handlers['default']
-		end
-		klass
-	end
+  def get_klass()
+    klass = false
+    if @handlers.include?(@role)
+      klass = @handlers[@role]
+    elsif @handlers.include?('default')
+      klass = @handlers['default']
+    end
+    klass
+  end
 
-	def run()
-		unless klass = get_klass
-			log "no handler for role"
-		else
-			begin
-				klass.send @event
-			rescue NoMethodError => e
-				log "event not implemented by class"
-			end
-		end
-	end
+  def run()
+    unless klass = get_klass
+      log "no handler for role"
+    else
+      begin
+        klass.send @event
+      rescue NoMethodError => e
+        log "event not implemented by class"
+      end
+    end
+  end
 end
--- spec/serf_handler_spec.rb
+++ spec/serf_handler_spec.rb
@@ -1,61 +1,61 @@
 require 'spec_helper'
 
 describe "SerfHandler" do
-	context "Check initialize process" do
-		before do
-			ENV['SERF_SELF_NAME'] = 'local'
-		  ENV['SERF_SELF_ROLE'] = 'web'
-		  ENV['SERF_EVENT'] = 'member-join'
-		end
+  context "Check initialize process" do
+    before do
+      ENV['SERF_SELF_NAME'] = 'local'
+      ENV['SERF_SELF_ROLE'] = 'web'
+      ENV['SERF_EVENT'] = 'member-join'
+    end
 
-	  it "has handler name as 'local'" do
-		  handler = SerfHandler.new
-	  	expect(handler.name).to eq('local')
-	  end
+    it "has handler name as 'local'" do
+      handler = SerfHandler.new
+      expect(handler.name).to eq('local')
+    end
 
-	  it "has handler role as 'web'" do
-	  	handler = SerfHandler.new
-	  	expect(handler.role).to eq('web')
-	  end
+    it "has handler role as 'web'" do
+      handler = SerfHandler.new
+      expect(handler.role).to eq('web')
+    end
 
-	  it "is set the event: 'member-join'" do
-	  	handler = SerfHandler.new
-	  	expect(handler.event).to eq('member_join')
-	  end
-	end
+    it "is set the event: 'member-join'" do
+      handler = SerfHandler.new
+      expect(handler.event).to eq('member_join')
+    end
+  end
 
-	context "Check user event processing" do
-		before do
-			ENV['SERF_SELF_NAME'] = 'local'
-		  ENV['SERF_SELF_ROLE'] = 'web'
-		  ENV['SERF_EVENT'] = 'user'
-		  ENV['SERF_USER_EVENT'] = 'deploy'
-		end
+  context "Check user event processing" do
+    before do
+      ENV['SERF_SELF_NAME'] = 'local'
+      ENV['SERF_SELF_ROLE'] = 'web'
+      ENV['SERF_EVENT'] = 'user'
+      ENV['SERF_USER_EVENT'] = 'deploy'
+    end
 
-	  it "has custome event: user" do
-	  	handler = SerfHandler.new
-	  	expect(handler.event).to eq('deploy')
-	  end
-	end
+    it "has custome event: user" do
+      handler = SerfHandler.new
+      expect(handler.event).to eq('deploy')
+    end
+  end
 end
 
 describe "SerfHandlerProxy" do
-	context "Check Processing Tags" do
-		before do
-			ENV['SERF_SELF_NAME'] = nil
+  context "Check Processing Tags" do
+    before do
+      ENV['SERF_SELF_NAME'] = nil
       ENV['SERF_TAG_ROLE'] = 'bob'
       ENV['SERF_EVENT'] = 'member-join'
     end
 
     it "has the role as bob" do
-    	handler = SerfHandlerProxy.new
-    	expect(handler.role).to eq('bob')
+      handler = SerfHandlerProxy.new
+      expect(handler.role).to eq('bob')
     end
-	end
+  end
 
-	context "Test negative cases" do
-		before do
-			ENV['SERF_TAG_ROLE'] = nil
+  context "Test negative cases" do
+    before do
+      ENV['SERF_TAG_ROLE'] = nil
       ENV['SERF_SELF_NAME'] = nil
       ENV['SERF_SELF_ROLE'] = nil
       ENV['SERF_EVENT'] = 'member-join'
@@ -65,63 +65,63 @@ describe "SerfHandlerProxy" do
       logger = double('Logger')
       expect(logger).to receive(:info).with("no handler for role").and_return(true)
       handler = SerfHandlerProxy.new(logger)
-    	handler.run
+      handler.run
     end
 
     it "has no method implemented" do
       logger = double('Logger')
       expect(logger).to receive(:info).with("event not implemented by class").and_return(true)
       handler = SerfHandlerProxy.new(logger)
-    	handler.register('default', SerfHandler.new)
-    	handler.run
+      handler.register('default', SerfHandler.new)
+      handler.run
     end
   end
 
   context "Test custom event" do
-  	before do
+    before do
       ENV['SERF_SELF_NAME'] = nil
       ENV['SERF_SELF_ROLE'] = nil
       ENV['SERF_EVENT'] = 'user'
-		  ENV['SERF_USER_EVENT'] = 'implemented'
-  	end
+      ENV['SERF_USER_EVENT'] = 'implemented'
+    end
 
-  	it "has method to process custom event" do
-  		handler = SerfHandlerProxy.new
-  		sample = double('SerfHandler')
-  		expect(sample).to receive(:implemented).and_return(true)
-  		handler.register('default', sample)
-  		handler.run
-  	end
+    it "has method to process custom event" do
+      handler = SerfHandlerProxy.new
+      sample = double('SerfHandler')
+      expect(sample).to receive(:implemented).and_return(true)
+      handler.register('default', sample)
+      handler.run
+    end
   end
 
   context "Test standard event when role is default" do
-  	before do
+    before do
       ENV['SERF_SELF_NAME'] = nil
       ENV['SERF_EVENT'] = 'member-join'
-  	end
+    end
 
-  	it "has method to process member_join event" do
-  		handler = SerfHandlerProxy.new
-  		sample = double('SerfHandler')
-  		expect(sample).to receive(:member_join).and_return(true)
-  		handler.register('default', sample)
-  		handler.run
-		end
-	end
+    it "has method to process member_join event" do
+      handler = SerfHandlerProxy.new
+      sample = double('SerfHandler')
+      expect(sample).to receive(:member_join).and_return(true)
+      handler.register('default', sample)
+      handler.run
+    end
+  end
 
   context "Test standard event when role is web" do
-  	before do
+    before do
       ENV['SERF_SELF_NAME'] = nil
       ENV['SERF_SELF_ROLE'] = 'web'
       ENV['SERF_EVENT'] = 'member-join'
-  	end
+    end
 
-		it "has method to process member_join event" do
-			handler = SerfHandlerProxy.new
-  		sample = double('SerfHandler')
-  		expect(sample).to receive(:member_join).and_return(true)
-  		handler.register('web', sample)
-  		handler.run
-  	end
+    it "has method to process member_join event" do
+      handler = SerfHandlerProxy.new
+      sample = double('SerfHandler')
+      expect(sample).to receive(:member_join).and_return(true)
+      handler.register('web', sample)
+      handler.run
+    end
   end
 end
